c37a20bc224323cc20242a576d64ba9b157aaf65
Anton <62050115+TohaSharp@users.noreply.github.com>
2020-05-06 17:50:42 +0300

    slider
    add SWIPER and BXSLIDSER

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
-<script>
-    var swiper = new Swiper('.swiper-container', {
-      spaceBetween: 30,
-      effect: 'fade',
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  </script>
+var swiper = new Swiper(".swiper-container", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+$(document).ready(function() {
+  $(".slider").bxSlider();
+});
